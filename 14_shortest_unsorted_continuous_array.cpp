9b1883917cd27468ecd754ce64d0f8e4e3424f62
Shiqi He <she010@uottawa.ca>
2020-07-09 10:10:28 -0400

    add another solution to the question 14

--- 14_shortest_unsorted_continuous_array.cpp
+++ 14_shortest_unsorted_continuous_array.cpp
@@ -1,3 +1,8 @@
+// Leetcode 581
+// Two solutions. 
+// 1. Copy and sort + compare with original. (Provided by the leetcode)
+// 2. Brute search: https://www.youtube.com/watch?v=UfBfr-VRYOU&feature=youtu.be
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -136,9 +141,104 @@ int findUnsortedSubarray_solution(std::vector<int>& nums) {
 	}
 }
 
+// Brute force solution. 
+// Time: O(n), space: O(1)
+int findUnsortedSubarray_brute(std::vector<int>& nums) {
+
+	int length = nums.size();
+
+	if(length == 1){
+		return 0;
+	}
+
+	int s{};
+	int e{};
+
+	int max{};
+	int min{};
+
+	bool pass = false;
+
+
+	for(int i = 0; i < length - 1; ++i){
+
+		if(nums[i] > nums[i + 1]){
+
+			s = i;
+			break;
+		}
+
+	}
+	
+	for(int j = (length - 1); j > 0; --j){
+
+		if( nums[j] < nums[j - 1]){	// violates
+
+			e = j;
+			break;
+		}
+
+	}
+
+	if(s == 0 && e == 0){	// already sorted
+
+		return 0;
+	}
+
+	// sort between [s, e]
+	// get the iterator
+	std::vector<int>::iterator it_s = (nums.begin() + s);
+	std::vector<int>::iterator it_e = (nums.end() - (length - 1 - e));
+	std::sort(it_s, it_e);
+
+	min = nums[s];	// minimum value of the sorted subarray. 
+	max = nums[e];
+
+	while(!pass){
+
+		pass = true;
+		for(int i = 0; i < s; ++i){
+
+
+			if(nums[i] > min){
+
+				s = i;
+
+				pass = false;
+
+				break;
+			}
+		}
+
+		for(int j = length - 1; j > e; --j){
+
+			if(nums[j] < max){
+
+				e = j;
+				pass = false;
+				break;
+			}
+
+		}
+
+		if(pass) break;
+
+		// sort again
+		std::vector<int>::iterator it_s = (nums.begin() + s);
+		std::vector<int>::iterator it_e = (nums.end() - (length - 1 - e));
+		std::sort(it_s, it_e);
+	
+		min = nums[s];	// minimum value of the sorted subarray. 
+		max = nums[e];
+	}
+
+	return (e - s + 1);
+
+}
+
 int main(){
 
-	std::vector<int> my_vector {1, 2, 3, 2, 5, 9};
+	std::vector<int> my_vector {2, 2, 3, 8, 7, 2, 1};
 
 //	int adjust = findUnsortedSubarray(my_vector); 
 //
@@ -150,9 +250,13 @@ int main(){
 //	}
 //	std::cout<< "\n";
 //
-	int adjust2 = findUnsortedSubarray_solution(my_vector); 
+//	int adjust2 = findUnsortedSubarray_solution(my_vector); 
+//
+//	std::cout << "Subarray length = " << adjust2 << "\n";
 
-	std::cout << "Subarray length = " << adjust2 << "\n";
+	int d3 = findUnsortedSubarray_brute(my_vector);
+
+	std::cout << "Subarray length = " << d3 << "\n";
 
 	for(auto& v : my_vector){
 
